33b1435ce09ca944fa778375d4995f7fcce17d12
jayashree <95606189+Jayashree-saravanan@users.noreply.github.com>
2023-04-22 23:16:40 +0200

    new

---
+++ src/Component/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+      <footer className="sticky-footer bg-white">
+        <div className="container my-auto">
+          <div className="copyright text-center my-auto">
+          </div>
+        </div>
+      </footer>
+    );
+  }
